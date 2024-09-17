d8f656a11275746ee5e51c0a32b5b800f514709f
Angus Yeh <a.yeh@auckland.ac.nz>
2024-09-17 19:58:46 +1200

    Add version lookup worst case scenario.

--- gopest/__init__.py
+++ gopest/__init__.py
@@ -1,7 +1,11 @@
 try:
-    # works if calling from an -e installation, or whenever from a git repo
+    # works if calling from an repo or an pip install -e installation
     from setuptools_scm import get_version
     __version__ = get_version()
 except LookupError:
-    # works if calling from other pip install methods
-    from ._version import version as __version__
+    try:
+        # works if calling from other pip install methods
+        from ._version import version as __version__
+    except ImportError:
+        # worst case scenario
+        __version__ = '0.0.0'
